a1b01abab4e4ef5097ef2286367f12fb831ed334
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-25 05:19:42 +0000

    Create culpepper.rb

---
+++ culpepper.rb
@@ -0,0 +1,17 @@
+require "NeoPathfinding"
+
+2.times do
+  get_statistics(:marine_antionette, "[ Who ] Marie Antionette [ What ] Is guillotined in the public square. [ When ] On October 16, 1793. [ Where ] At the scaffold in the city of Paris. [ Why ] Because she said let them eat cake, when the French people needed Bread. [ How ] An angular and sharp eighty pound blade fell on her neck, taking her head right off.",
+                 :anna_boleyn,       "[ Who ] Ann Boleyn [ What ] Is beheaded by sword. [ When ] 19 May 1536 [ Where ] On a private scaffold within the castle. [ Why ] She was framed for committing treason against the crown. [ How ] With a sword brandished by a French excutioner imported from Paris.",
+                 :catherine_howard, "[ Who ] Catherine Howard [ What] is beheaded by ax. [ When ] February 13, 1542 [ Where ] On a private scaffold within the castle. [ Why ] She was caught cheating by Henry The VIII.")
+               
+                 dynamic_reward_allocation
+end
+
+2.times do
+  get_statistics(:marine_antionette, "[ Who ] Marie Antionette [ What ] Is guillotined in the public square. [ When ] On October 16, 1793. [ Where ] At the scaffold in the city of Paris. [ Why ] Because she said let them eat cake, when the French people needed Bread. [ How ] An angular and sharp eighty pound blade fell on her neck, taking her head right off.",
+                 :anna_boleyn,       "[ Who ] American Civil War [ What ] Is fought between the confederates and union. [ When ] Between 1861 to 1864. [ Where ] In various states split along the Mason-Dixon Line. [ Why ] The simple history is that the South wanted to split from the Union do to slavery. [ How ] Using canons, early trench warfare, and an evolution of early Submarines.",
+                 :catherine_howard, "[ Who ] Catherine Howard [ What] is beheaded by ax. [ When ] February 13, 1542 [ Where ] On a private scaffold within the castle. [ Why ] She was caught cheating by Henry The VIII.")
+               
+                 dynamic_reward_allocation
+end
